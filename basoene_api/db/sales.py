d4f79986d458150167457b00438747645cd32dc1
Eniatorudabo Abomaye <abomayesan@gmail.com>
2024-02-05 00:09:14 +0000

    added db/sales module

--- basoene_api/db/sales.py
+++ basoene_api/db/sales.py
@@ -1,14 +1,16 @@
 from sqlmodel import Session, select
-from basoene_api.models.products import ProductSales, Products, SalesPost, engine
+from basoene_api.models.products import ProductSales, Products, SalesPost
+from datetime import date
 from typing import Generator
 
 class NoSalesError(Exception):
     ...
 
 
-def get_session() -> Generator[Session, None, None]:
-    with Session(engine) as session:
-        yield session
+def db_sales(session: Session):
+    query = select(Products.product_name, Products.unit_price, ProductSales).where(Products.id == ProductSales.product_id)
+    result = session.exec(query).all()
+    return result
 
 
 def db_get_todays_sale(session: Session):
@@ -24,3 +26,27 @@ def db_get_todays_sale(session: Session):
         raise NoSalesError("No sales have been made today")
 
     return sales
+
+
+def db_get_sales(date: str, session: Session):
+
+    sales = session.exec(
+        (select(Products.product_name, Products.unit_price, ProductSales)
+         .where(Products.id == ProductSales.product_id)
+         .where(ProductSales.sale_date == date)
+         )
+    ).all()
+
+    if len(sales) < 1:
+        raise NoSalesError("No sales have been made today")
+
+    return sales
+
+
+def db_add_sales(sales: SalesPost, session: Session):
+    db_sales = ProductSales.from_orm(sales)
+    session.add(db_sales)
+    session.commit()
+    session.refresh(db_sales)
+
+    return db_sales
