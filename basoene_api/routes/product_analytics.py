a1a7d67a3fe47b5d093aa1eefee709b8ac2aaff8
Eniatorudabo Abomaye <abomayesan@gmail.com>
2024-02-15 23:17:12 +0000

    added db/room_analytics and db/product_analytics modules

--- basoene_api/routes/product_analytics.py
+++ basoene_api/routes/product_analytics.py
@@ -5,6 +5,14 @@ from typing import Generator
 from datetime import date
 from basoene_api.models.products import Products, ProductSales, engine
 from basoene_api.models.product_analysis import DateResults, ProductResults, HourResults, DayResults, MonthResults, SalesSummary
+from basoene_api.db.product_analytics import(
+     db_product_analysis,
+     db_trend_analysis,
+     db_month_analysis,
+     db_daily_analysis,
+     db_hourly_analysis,
+     db_summary_analysis
+)
 
 router = APIRouter()
 
@@ -14,101 +22,34 @@ def get_session() -> Generator[Session, None, None]:
         yield session
 
 @router.get("/analysis/product", response_model=list[ProductResults])
-async def analysis(session: Session = Depends(get_session)):
-     query = (select(Products.product_name, 
-                     func.sum(ProductSales.quantity * Products.unit_price).label("total"))
-                    .where(Products.id == ProductSales.product_id)
-                    .group_by(Products.product_name)
-                    .order_by("total")
-                )
-     result = session.exec(query).all()
-     return result
+async def product_analysis(session: Session = Depends(get_session)):
+     return db_product_analysis(session)
 
 
 @router.get("/analysis/product/date", response_model=list[DateResults])
-async def analysis(session: Session = Depends(get_session)):
-     query = (select(ProductSales.sale_date, 
-                     func.sum(ProductSales.quantity * Products.unit_price).label("total"))
-                    .where(Products.id == ProductSales.product_id)
-                    .group_by(ProductSales.sale_date)
-                )
-     result = session.exec(query).all()
-     return result
+async def trend_analysis(session: Session = Depends(get_session)):
+    return db_trend_analysis(session)
 
 
 @router.get("/analysis/product/month", response_model=list[MonthResults])
-async def analysis(session: Session = Depends(get_session)):
-     query = (select(extract("month",ProductSales.sale_date).label("month"),
-                      func.sum(ProductSales.quantity * Products.unit_price).label("total"))
-                        .where(Products.id == ProductSales.product_id)
-                        .group_by("month")
-                        .order_by("month")
-                    )
-     result = session.exec(query).all()
-
-     return result
+async def month_analysis(session: Session = Depends(get_session)):
+     return db_month_analysis(session)
 
 
 
 @router.get("/analysis/product/day", response_model=list[DayResults])
-async def analysis(session: Session = Depends(get_session)):
-     query = (select(extract("dow",ProductSales.sale_date).label("day"),
-                      func.sum(ProductSales.quantity * Products.unit_price).label("total"))
-                        .where(Products.id == ProductSales.product_id)
-                        .group_by("day")
-                        .order_by("day")
-                    )
-     result = session.exec(query).all()
-
-     return result
+async def daily_analysis(session: Session = Depends(get_session)):
+     return db_daily_analysis(session)
 
 
 @router.get("/analysis/product/hour", response_model=list[HourResults])
-async def analysis(session: Session = Depends(get_session)):
-     query = (select(extract("hour", ProductSales.time).label("hour"),
-                     func.sum(ProductSales.quantity * Products.unit_price).label("total")
-                     )
-                    .where(Products.id == ProductSales.product_id)
-                    .group_by("hour")
-                    .order_by("total")
-
-                    )
-     result = session.exec(query).all()
-     return result
+async def hourly_analysis(session: Session = Depends(get_session)):
+     return db_hourly_analysis(session)
 
 
 @router.get("/analysis/product/summary", response_model=SalesSummary)
-async def analysis(session: Session = Depends(get_session)):
-     total_sales = (select(
-                      func.sum(
-                         ProductSales.quantity * Products.unit_price
-                     ).label("total_sales")
-                     )
-                    .where(Products.id == ProductSales.product_id)
-                    )
-     
-     daily_sales = (select(ProductSales.sale_date,
-                      func.sum(
-                         ProductSales.quantity * Products.unit_price
-                     ).label("total")
-                     )
-                .where(Products.id == ProductSales.product_id)
-                .group_by(ProductSales.sale_date)
-                )
-     daily_average = select(func.round(
-                            func.avg(daily_sales.c.total), 2
-                            ).label("avg_daily_sales")
-                        )
-     
-     total_quantity = (select(func.sum(ProductSales.quantity).label("total_drinks")))
-     
-
-     result = {"total_sales": session.exec(total_sales).first(),
-               "avg_daily_sales": session.exec(daily_average).first(),
-               "total_products_sold": session.exec(total_quantity).first()
-               }
-
-     return result
+async def summary_analysis(session: Session = Depends(get_session)):
+     return db_summary_analysis(session)
 
 
 # Return analytics results for specific years.
@@ -117,109 +58,32 @@ async def analysis(session: Session = Depends(get_session)):
 # The below routes return data for the year passed as ann argument.
 
 @router.get("/analysis/product/{year}", response_model=list[ProductResults])
-async def analysis(year: str, session: Session = Depends(get_session)):
-     query = (select(Products.product_name, 
-                     func.sum(ProductSales.quantity * Products.unit_price).label("total"))
-                    .where(Products.id == ProductSales.product_id)
-                    .where(extract("year", ProductSales.sale_date) == year)
-                    .group_by(Products.product_name)
-                    .order_by("total")
-                )
-     result = session.exec(query).all()
-     return result
+async def product_analysis(year: str, session: Session = Depends(get_session)):
+    return db_product_analysis(session, year)
 
 
 @router.get("/analysis/product/date/{year}", response_model=list[DateResults])
-async def analysis(year: str, session: Session = Depends(get_session)):
-     query = (select(ProductSales.sale_date, 
-                     func.sum(ProductSales.quantity * Products.unit_price).label("total"))
-                    .where(Products.id == ProductSales.product_id)
-                    .where(extract("year", ProductSales.sale_date) == year)
-                    .group_by(ProductSales.sale_date)
-                )
-     result = session.exec(query).all()
-
-     return result
+async def trend_analysis(year: str, session: Session = Depends(get_session)):
+     return db_trend_analysis(session, year)
 
 
 @router.get("/analysis/product/month/{year}", response_model=list[MonthResults])
-async def analysis(year: str, session: Session = Depends(get_session)):
-     query = (select(extract("month",ProductSales.sale_date).label("month"),
-                      func.sum(ProductSales.quantity * Products.unit_price).label("total"))
-                        .where(Products.id == ProductSales.product_id)
-                        .where(extract("year", ProductSales.sale_date) == year)
-                        .group_by("month")
-                        .order_by("month")
-                    )
-     result = session.exec(query).all()
-
-     return result
+async def month_analysis(year: str, session: Session = Depends(get_session)):
+    return db_month_analysis(session, year)
 
 
 @router.get("/analysis/product/day/{year}", response_model=list[DayResults])
-async def analysis(year: str, session: Session = Depends(get_session)):
-     query = (select(extract("dow",ProductSales.sale_date).label("day"),
-                      func.sum(ProductSales.quantity * Products.unit_price).label("total"))
-                        .where(Products.id == ProductSales.product_id)
-                        .where(extract("year", ProductSales.sale_date) == year)
-                        .group_by("day")
-                        .order_by("day")
-                    )
-     result = session.exec(query).all()
-
-     return result
+async def daily_analysis(year: str, session: Session = Depends(get_session)):
+     return db_daily_analysis(session, year)
 
 
 @router.get("/analysis/product/hour/{year}", response_model=list[HourResults])
-async def analysis(year: str, session: Session = Depends(get_session)):
-     query = (select(extract("hour", ProductSales.time).label("hour"),
-                     func.sum(ProductSales.quantity * Products.unit_price).label("total")
-                     )
-                    .where(Products.id == ProductSales.product_id)
-                    .where(extract("year", ProductSales.sale_date) == year)
-                    .group_by("hour")
-                    .order_by("total")
-
-                    )
-     result = session.exec(query).all()
-     return result
+async def hourly_analysis(year: str, session: Session = Depends(get_session)):
+     return db_hourly_analysis(session, year)
 
 
 @router.get("/analysis/product/summary/{year}", response_model=SalesSummary)
-async def analysis(year: str, session: Session = Depends(get_session)):
-     total_sales = (select(
-                      func.sum(
-                         ProductSales.quantity * Products.unit_price
-                     ).label("total_sales")
-                     )
-                .where(Products.id == ProductSales.product_id)
-                .where(extract("year",ProductSales.sale_date) == year)
-                )
-     
-     daily_sales = (select(ProductSales.sale_date,
-                      func.sum(
-                         ProductSales.quantity * Products.unit_price
-                     ).label("total")
-                     )
-                .where(Products.id == ProductSales.product_id)
-                .where(extract("year",ProductSales.sale_date) == year)
-                .group_by(ProductSales.sale_date)
-                )
-     daily_average = select(func.round(
-                            func.avg(daily_sales.c.total), 2
-                            ).label("avg_daily_sales")
-                        )
-     
-     total_quantity = (select(func.sum(ProductSales.quantity).label("total_drinks"))
-                .where(extract("year",ProductSales.sale_date) == year))
-     
-
-     result = {"total_sales": session.exec(total_sales).first(),
-               "avg_daily_sales": session.exec(daily_average).first(),
-               "total_products_sold": session.exec(total_quantity).first()
-               }
-
-     return result
-
+async def summary_analysis(year: str, session: Session = Depends(get_session)):
+     return db_summary_analysis(session, year)
 
 
--- basoene_api/routes/room_analytics.py
+++ basoene_api/routes/room_analytics.py
@@ -5,6 +5,13 @@ from typing import Generator
 from datetime import date
 from basoene_api.models.rooms import Rooms, Bookings, engine
 from basoene_api.models.room_analysis import DateResults, RoomResults, HourCount, DayResults, BookingSummary
+from basoene_api.db.room_analytics import (
+    db_room_analysis,
+    db_trend_analysis,
+    db_daily_analysis,
+    db_hourly_analysis,
+    db_summary_analysis
+)
 
 router = APIRouter()
 
@@ -14,116 +21,32 @@ def get_session() -> Generator[Session, None, None]:
 
 
 @router.get("/analysis/room", response_model=list[RoomResults])
-async def analysis(session: Session = Depends(get_session)):
-    query = (select(Rooms.room_name,
-                     func.sum(
-                         case(
-                             (func.lower(Bookings.booking_type) == "short", Rooms.price_short),
-                             else_ = Rooms.price_full
-                             )
-                     ).label("revenue")
-                  )
-                .where(Bookings.room_id == Rooms.id)
-                .group_by(Rooms.room_name)
-                .order_by("revenue")
-         
-        )
-    result = session.exec(query).all()
-    return result
+async def room_analysis(session: Session = Depends(get_session)):
+    return db_room_analysis(session)
 
 
 @router.get("/analysis/room/date", response_model=list[DateResults])
-async def analysis(session: Session = Depends(get_session)):
-    query = (select(Bookings.booking_date, 
-                      func.sum(
-                         case(
-                             (func.lower(Bookings.booking_type) == "short", Rooms.price_short),
-                             else_ = Rooms.price_full
-                             )
-                     ).label("revenue"))
-                    .where(Rooms.id == Bookings.room_id)
-                    .group_by(Bookings.booking_date)
-                )
-    result = session.exec(query).all()
-
-    return result
+async def trend_analysis(session: Session = Depends(get_session)):
+    return db_trend_analysis(session)
 
 
 
 @router.get("/analysis/room/daily_revenue", response_model=list[DayResults])
-async def analysis(session: Session = Depends(get_session)):
+async def daily_analysis(session: Session = Depends(get_session)):
      
-    query = (select(extract("dow",Bookings.booking_date).label("day"),
-                      func.sum(
-                         case(
-                             (func.lower(Bookings.booking_type) == "short", Rooms.price_short),
-                             else_ = Rooms.price_full
-                             )
-                     ).label("revenue")
-                     )
-                .where(Rooms.id == Bookings.room_id)
-                .group_by("day")
-                .order_by("day")
-                )
-    
-    result = session.exec(query).all()#
-    return result
+    return db_daily_analysis(session)
 
     
 
 @router.get("/analysis/room/hour_count", response_model=list[HourCount])
 async def analysis(session: Session = Depends(get_session)):
-     
-    query = (select(extract("hour", Bookings.time).label("hour"),
-                    func.count(Bookings.time).label("booking_count"))
-            .group_by("hour")
-            .order_by("booking_count")
-            )  
-         
-    result = session.exec(query).all()
-    return result
+    return db_hourly_analysis(session)
 
 
 
 @router.get("/analysis/room/summary", response_model=BookingSummary)
 async def analysis(session: Session = Depends(get_session)):
-     
-    total_revenue = (select(func.sum(
-                         case(
-                             (func.lower(Bookings.booking_type) == "short", Rooms.price_short),
-                             else_ = Rooms.price_full
-                             )
-                     ).label("revenue")
-                  )
-         .where(Bookings.room_id == Rooms.id)  
-        )
-    
-
-
-    daily_revenue = (select(func.sum(
-                         case(
-                             (func.lower(Bookings.booking_type) == "short", Rooms.price_short),
-                             else_ = Rooms.price_full
-                             )
-                        ).label("revenue")
-                        )
-                .where(Bookings.room_id == Rooms.id)
-                .group_by(Bookings.booking_date)
-                )
-    avg_daily_revenue = select(func.round(
-                        func.avg(daily_revenue.c.revenue), 2
-                        ).label("avg_daily_revenue")
-                        )
-
-    total_bookings = (select(func.count(Bookings.time)))
-
-         
-    result = {"total_revenue": session.exec(total_revenue).first(),
-              "avg_daily_revenue": session.exec(avg_daily_revenue).first(),
-              "total_bookings": session.exec(total_bookings).first()
-              }
-
-    return result
+    return db_summary_analysis(session)
 
 # Return analytics results for specific years.
 # Contains the same data as the routes above
@@ -131,124 +54,29 @@ async def analysis(session: Session = Depends(get_session)):
 # The below routes return data for the year passed as ann argument.
 
 @router.get("/analysis/room/{year}", response_model=list[RoomResults])
-async def analysis(year: str, session: Session = Depends(get_session)):
-    query = (select(Rooms.room_name,
-                     func.sum(
-                         case(
-                             (func.lower(Bookings.booking_type) == "short", Rooms.price_short),
-                             else_ = Rooms.price_full
-                             )
-                     ).label("revenue")
-                  )
-                .where(Bookings.room_id == Rooms.id)
-                .where(extract("year",Bookings.booking_date) == year)
-                .group_by(Rooms.room_name)
-                .order_by("revenue")
-         
-        )
-    result = session.exec(query).all()
-    return result
+async def room_analysis(year: str, session: Session = Depends(get_session)):
+    return db_room_analysis(session, year)
 
 
 @router.get("/analysis/room/date/{year}", response_model=list[DateResults])
-async def analysis(year: str, session: Session = Depends(get_session)):
-    query = (select(Bookings.booking_date, 
-                      func.sum(
-                         case(
-                             (func.lower(Bookings.booking_type) == "short", Rooms.price_short),
-                             else_ = Rooms.price_full
-                             )
-                     ).label("revenue"))
-                    .where(Rooms.id == Bookings.room_id)
-                    .where(extract("year", Bookings.booking_date) == year)
-                    .group_by(Bookings.booking_date)
-                )
-    result = session.exec(query).all()
-
-    return result
+async def trend_analysis(year: str, session: Session = Depends(get_session)):
+    return db_trend_analysis(session, year)
 
 
 
 @router.get("/analysis/room/daily_revenue/{year}", response_model=list[DayResults])
-async def analysis(year: str, session: Session = Depends(get_session)):
-     
-    query = (select(extract("dow",Bookings.booking_date).label("day"),
-                      func.sum(
-                         case(
-                             (func.lower(Bookings.booking_type) == "short", Rooms.price_short),
-                             else_ = Rooms.price_full
-                             )
-                     ).label("revenue")
-                     )
-                .where(Rooms.id == Bookings.room_id)
-                .where(extract("year",Bookings.booking_date) == year)
-                .group_by("day")
-                .order_by("day")
-                )
-    
-    result = session.exec(query).all()#
-    return result
+async def daily_analysis(year: str, session: Session = Depends(get_session)):
+    return db_daily_analysis(session, year)
 
     
 
 @router.get("/analysis/room/hour_count/{year}", response_model=list[HourCount])
 async def analysis(year: str, session: Session = Depends(get_session)):
-     
-    query = (select(extract("hour", Bookings.time).label("hour"),
-                    func.count(Bookings.time).label("booking_count"))
-            .where(extract("year", Bookings.booking_date) == year)
-            .group_by("hour")
-            .order_by("booking_count")
-            )  
-         
-    result = session.exec(query).all()
-    return result
+    return db_hourly_analysis(session, year)
 
 
 @router.get("/analysis/room/summary/{year}", response_model=BookingSummary)
 async def analysis(year: str, session: Session = Depends(get_session)):
-     
-    total_revenue = (select(func.sum(
-                         case(
-                             (func.lower(Bookings.booking_type) == "short", Rooms.price_short),
-                             else_ = Rooms.price_full
-                             )
-                     ).label("revenue")
-                  )
-         .where(Bookings.room_id == Rooms.id)
-         .where(extract("year",Bookings.booking_date) == year)   
-        )
-    
-
-
-    daily_revenue = (select(func.sum(
-                         case(
-                             (func.lower(Bookings.booking_type) == "short", Rooms.price_short),
-                             else_ = Rooms.price_full
-                             )
-                        ).label("revenue")
-                        )
-                .where(Bookings.room_id == Rooms.id)
-                .where(extract("year",Bookings.booking_date) == year)
-                .group_by(Bookings.booking_date)
-                )
-    avg_daily_revenue = select(func.round(
-                        func.avg(daily_revenue.c.revenue), 2
-                        ).label("avg_daily_revenue")
-                        )
-
-    total_bookings = (select(func.count(Bookings.time))
-              .where(extract("year",Bookings.booking_date) == year)
-              )
-
-         
-    result = {"total_revenue": session.exec(total_revenue).first(),
-              "avg_daily_revenue": session.exec(avg_daily_revenue).first(),
-              "total_bookings": session.exec(total_bookings).first()
-              }
-
-    return result
-
-
+     return db_summary_analysis(session, year)
 
 
